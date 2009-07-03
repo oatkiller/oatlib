5dd02903cf50ae54ed9d83dd60e620ed51a5bf6a
robert <robert@oatlab.com>
2009-07-03 16:14:57 -0400

    parse works

--- src/json/parse.js
+++ src/json/parse.js
@@ -1,4 +1,3 @@
-// this should usually be useless
 //= require <json/reference>
 (function () {
 	var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
