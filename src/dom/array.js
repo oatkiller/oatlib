3bbe2074ddeda26a471310937fa3fd04d933ace3
robert <robert@oatlab.com>
2009-07-06 22:42:33 -0400

    using new doll technique with dom/array now

--- src/dom/array.js
+++ src/dom/array.js
@@ -2,18 +2,25 @@
 //= require <array>
 //= require <map>
 
-$$_dom[$array] = function () { // try to use slice to get an array from an HTML elements collection. if this works, use slice for the array fn, else use an fn that iterates over the array like object and builds a new array incrementally. IE should get the second fn, others should get the first. generally.
+(function () {
+
 	var testDiv = $$document[$createElement]('div'),
-	fn;
+	fn = function () {
+		try {// try to use slice to get an array from an HTML elements collection. if this works, use slice for the array fn, else use an fn that iterates over the array like object and builds a new array incrementally. IE should get the second fn, others should get the first. generally.
+			$$_array(testDiv[$childNodes]);
+			fn = $$_dom[$array] = $$_array;
+		} catch (e) {
+			var o$map = o[$map];
+			fn = function (arrayLike) {
+				return $$_array_prototype[o$map][$apply](arrayLike,function (a) {return a;});
+			};
+		}
+		return fn[$apply]($$null,arguments);
+	};
 	testDiv[$innerHTML] = 'a<d></b>';
-	try {
-		var $$_array = o[$array];
-		$$_array(testDiv[$childNodes]);
-		fn = $$_dom[$array] = $$_array;
-	} catch (e) {
-		fn = $$_dom[$array] = function (arrayLike) {
-			return $$_array_prototype[o[$map]][$apply](arrayLike,function (a) {return a;});
-		};
-	}
-	return fn[$apply]($$null,arguments);
-};
+
+	$$_dom[$array] = function () {
+		return fn[$apply]($$null,arguments);
+	};
+
+})();
