684568fdf3e38bdcaf6b7ff7ff69a0a553f5603f
robert <robert@oatlab.com>
2009-07-07 21:35:23 -0400

    wrote units for, and fixed bugs in get_abstraction. its working now i think

--- src/dom/event/get_abstraction.js
+++ src/dom/event/get_abstraction.js
@@ -1,13 +1,21 @@
+//= require <builder>
 //= require <dom/event/reference>
 //= require <dom/event/get_event>
+//= require <dom/event/get_button>
+//= require <dom/event/get_key>
+//= require <dom/event/get_mouse_coordinates>
+//= require <dom/event/get_related_target>
+//= require <dom/event/get_target>
 //= require <curry>
 //= require <array>
 (function () {
  	var curry_with_this_dot_event = function (fn) {
-		return fn[$apply](this,[this[$event]][$concat]($$_array(arguments)));
+		return function () {
+			return fn[$apply](this,[this[$event]][$concat]($$_array(arguments)));
+		};
 	},
 	getter = $$_builder({
-		get_event = curry_with_this_dot_event($$_dom_event_get_event),
+		get_event: curry_with_this_dot_event($$_dom_event_get_event),
 		get_button: curry_with_this_dot_event($$_dom_event_get_button),
 		get_key: curry_with_this_dot_event($$_dom_event_get_key),
 		get_mouse_coordinates: curry_with_this_dot_event($$_dom_event_get_mouse_coordinates),
