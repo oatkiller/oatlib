a60fec1d6498391e54be010a3365481d0b5cbded
robert <robert@oatlab.com>
2009-07-12 00:58:36 -0400

    added a test for filter

--- units/filter.tests.js
+++ units/filter.tests.js
@@ -1,12 +1,3 @@
-tests.core = [
-	{
-		name: 'core',
-		setUp: function () {o = window['http://oatlab.com/oatlib/v2'];},
-		'test core': function () {
-			Assert.areEqual('http://oatlab.com/oatlib/v2',o);
-		}
-	}
-];
 tests.filter = [
 	{
 		name: 'filter',
@@ -19,3 +10,4 @@ tests.filter = [
 		}
 	}
 ];
+
