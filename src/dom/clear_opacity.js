50ce284d94918e171b96389ffbf83aefa5db97a5
robert <robert@oatlab.com>
2009-07-13 18:38:08 -0400

    got clear and set opacity working for now.. not happy with it though

--- src/dom/clear_opacity.js
+++ src/dom/clear_opacity.js
@@ -1,13 +1,4 @@
 //= require <dom/reference>
-(function () {
-	var fn = function (node) {
-		return (fn = $$_dom_clear_opacity = $$_dom[$clear_opacity] =  $opacity in node[$style] ? function (node) {
-			return node[$style][$opacity] = emptyString;
-		} : function (node) {
-			return node[$style][$filter] = 'alpha(opacity=100)';
-		})[$apply](this,arguments);
-	};
-	$$_dom_clear_opacity = $$_dom[$clear_opacity] = function (node) {
-		return fn[$apply](this,arguments);
-	};
-})();
+$$_dom_clear_opacity = $$_dom[$clear_opacity] = function (node) {
+	return node[$style][$filter] = node[$style][$opacity] = emptyString;
+};
--- src/dom/set_opacity.js
+++ src/dom/set_opacity.js
@@ -1,6 +1,15 @@
 //= require <dom/reference>
 //= require <string>
-$$_dom_set_opacity = $$_dom[$set_opacity] = function (node,value) {
-	node[$style][$opacity] = value / 10;
-	node[$style][$filter] = $$_string('alpha(opacity=',value * 10,')');
-};
+
+(function () {
+	var fn = function (node) {
+		return (fn = $$_dom_set_opacity = $$_dom[$set_opacity] = node[$filters] ? function (node,value) {
+			return node[$style][$filter] = $$_string('alpha(opacity=',value * 10,')');
+		} : function (node,value) {
+			return node[$style][$opacity] = value / 10;
+		})[$apply](this,arguments);
+	};
+	$$_dom_set_opacity = $$_dom[$set_opacity] = function () {
+		return fn[$apply](this,arguments);
+	};
+})();
