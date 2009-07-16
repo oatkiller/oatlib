2e699ec1ba1bbbd2c5807cf6500a3f7aeee4a856
robert <robert@oatlab.com>
2009-07-16 15:49:07 -0400

    fixed stringify a little

--- src/json/parse.js
+++ src/json/parse.js
@@ -1,81 +1,78 @@
 //= require <json/reference>
-(function () {
-	var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+$$_json_stringify = $$_json.parse = function (text, reviver) {
 
-	$$_json_parse = $$_json.parse = function (text, reviver) {
+// The parse method takes a text and an optional reviver function, and returns
+// a JavaScript value if the text is a valid JSON text.
 
-	// The parse method takes a text and an optional reviver function, and returns
-	// a JavaScript value if the text is a valid JSON text.
+				var j;
 
-			var j,
-			walk = function (holder, key) {
+				function walk(holder, key) {
 
-				// The walk method is used to recursively walk the resulting structure so that modifications can be made.
+// The walk method is used to recursively walk the resulting structure so
+// that modifications can be made.
 
-				var k, v, value = holder[key];
-				if (value && typeof value === $object) {
-					for (k in value) {
-						if ($$_hasOwnProperty.call(value, k)) {
-							v = walk(value, k);
-							if (v !== undefined) {
-								value[k] = v;
-							} else {
-								delete value[k];
-							}
+						var k, v, value = holder[key];
+						if (value && typeof value === 'object') {
+								for (k in value) {
+										if (Object.hasOwnProperty.call(value, k)) {
+												v = walk(value, k);
+												if (v !== undefined) {
+														value[k] = v;
+												} else {
+														delete value[k];
+												}
+										}
+								}
 						}
-					}
+						return reviver.call(holder, key, value);
 				}
-				return reviver.call(holder, key, value);
-			};
 
 
-			// Parsing happens in four stages. In the first stage, we replace certain Unicode characters with escape sequences. JavaScript handles many characters incorrectly, either silently deleting them, or treating them as line endings.
+// Parsing happens in four stages. In the first stage, we replace certain
+// Unicode characters with escape sequences. JavaScript handles many characters
+// incorrectly, either silently deleting them, or treating them as line endings.
 
-			cx.lastIndex = 0;
-			if (cx.test(text)) {
-					text = text.replace(cx, function (a) {
-							return '\\u' +
-									('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-					});
-			}
+				cx.lastIndex = 0;
+				if (cx.test(text)) {
+						text = text.replace(cx, function (a) {
+								return '\\u' +
+										('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+						});
+				}
 
-	// In the second stage, we run the text against regular expressions that look
-	// for non-JSON patterns. We are especially concerned with '()' and 'new'
-	// because they can cause invocation, and '=' because it can cause mutation.
-	// But just to be safe, we want to reject all unexpected forms.
+// In the second stage, we run the text against regular expressions that look
+// for non-JSON patterns. We are especially concerned with '()' and 'new'
+// because they can cause invocation, and '=' because it can cause mutation.
+// But just to be safe, we want to reject all unexpected forms.
 
-	// We split the second stage into 4 regexp operations in order to work around
-	// crippling inefficiencies in IE's and Safari's regexp engines. First we
-	// replace the JSON backslash pairs with '@' (a non-JSON character). Second, we
-	// replace all simple value tokens with ']' characters. Third, we delete all
-	// open brackets that follow a colon or comma or that begin the text. Finally,
-	// we look to see that the remaining characters are only whitespace or ']' or
-	// ',' or ':' or '{' or '}'. If that is so, then the text is safe for eval.
+// We split the second stage into 4 regexp operations in order to work around
+// crippling inefficiencies in IE's and Safari's regexp engines. First we
+// replace the JSON backslash pairs with '@' (a non-JSON character). Second, we
+// replace all simple value tokens with ']' characters. Third, we delete all
+// open brackets that follow a colon or comma or that begin the text. Finally,
+// we look to see that the remaining characters are only whitespace or ']' or
+// ',' or ':' or '{' or '}'. If that is so, then the text is safe for eval.
 
-
-			if (/^[\],:{}\s]*$/.
+				if (/^[\],:{}\s]*$/.
 test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@').
 replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
 replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
 
+// In the third stage we use the eval function to compile the text into a
+// JavaScript structure. The '{' operator is subject to a syntactic ambiguity
+// in JavaScript: it can begin a block or an object literal. We wrap the text
+// in parens to eliminate the ambiguity.
 
+						j = eval('(' + text + ')');
 
-	// In the third stage we use the eval function to compile the text into a
-	// JavaScript structure. The '{' operator is subject to a syntactic ambiguity
-	// in JavaScript: it can begin a block or an object literal. We wrap the text
-	// in parens to eliminate the ambiguity.
+// In the optional fourth stage, we recursively walk the new structure, passing
+// each name/value pair to a reviver function for possible transformation.
 
-					j = eval('(' + text + ')');
+						return typeof reviver === 'function' ?
+								walk({'': j}, '') : j;
+				}
 
-	// In the optional fourth stage, we recursively walk the new structure, passing
-	// each name/value pair to a reviver function for possible transformation.
+// If the text is not JSON parseable, then a SyntaxError is thrown.
 
-					return typeof reviver === $function ?
-							walk({'': j}, emptyString) : j;
-			}
-
-	// If the text is not JSON parseable, then a SyntaxError is thrown.
-
-			//throw new SyntaxError('JSON.parse');
-	};
-})();
+				throw new SyntaxError('JSON.parse');
+		};
--- src/json/stringify.js
+++ src/json/stringify.js
@@ -7,107 +7,91 @@
 //= require <language/prototypes/boolean>
 // this is terrible stuff
 (function () {
-
-	var format = function (n) {
-		// Format integers to have at least two digits.
+	var f = function (n) {
 		return n < 10 ? '0' + n : n;
-	},
-	valueOf = function () {return this.valueOf();},
+	};
+
+
+	$$_store($$_language_prototypes_date,$toJSON,function (key) {
+		return isFinite(this.valueOf()) ?
+			this.getUTCFullYear()   + '-' +
+			f(this.getUTCMonth() + 1) + '-' +
+			f(this.getUTCDate())      + 'T' +
+			f(this.getUTCHours())     + ':' +
+			f(this.getUTCMinutes())   + ':' +
+			f(this.getUTCSeconds())   + 'Z' : null;
+	});
+
+	(function () {
+		var p = function (key) {
+			return this.valueOf();
+		};
+		$$_store($$_language_prototypes_string,$toJSON,p);
+		$$_store($$_language_prototypes_number,$toJSON,p);
+		$$_store($$_language_prototypes_boolean,$toJSON,p);
+	})();
+
+	var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
 	escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
 	gap,
 	indent,
-	meta = {    // table of character substitutions
-			'\b': '\\b',
-			'\t': '\\t',
-			'\n': '\\n',
-			'\f': '\\f',
-			'\r': '\\r',
-			'"' : '\\"',
-			'\\': '\\\\'
+	meta = {    
+		'\b': '\\b',
+		'\t': '\\t',
+		'\n': '\\n',
+		'\f': '\\f',
+		'\r': '\\r',
+		'"' : '\\"',
+		'\\': '\\\\'
 	},
-	quote,
-	str,
 	rep;
 
-	$$_store($$_language_prototypes_date,$toJSON,function (key) {
-		var that = this;
-
-		return isFinite(that.valueOf()) ? $$_string(
-			that.getUTCFullYear(),'-',
-			format(that.getUTCMonth() + 1),'-',
-			format(that.getUTCDate()),'T',
-			format(that.getUTCHours()),':',
-			format(that.getUTCMinutes()),':',
-			format(that.getUTCSeconds()),'Z') : null;
-	});
-
-	$$_store($$_language_prototypes_string,$toJSON,valueOf);
-	$$_store($$_language_prototypes_number,$toJSON,valueOf);
-	$$_store($$_language_prototypes_boolean,$toJSON,valueOf);
-
-	quote = function (string) {
-
-		// If the string contains no control characters, no quote characters, and no
-		// backslash characters, then we can safely slap some quotes around it.
-		// Otherwise we must also replace the offending characters with safe escape
-		// sequences.
-
+	var quote = function (string) {
 		escapable.lastIndex = 0;
-		return escapable.test(string) ?
-			$$_string('"',string.replace(escapable, function (a) {
-				var c = meta[a];
-				return typeof c === $string ? c :
-					'\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-			}),'"') :
-			$$_string('"',string,'"');
+		return escapable.test(string) ?  '"' + string.replace(escapable, function (a) {
+			var c = meta[a];
+			return typeof c === 'string' ? c : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+		}) + '"' :
+		'"' + string + '"';
 	};
 
-	str = function (key, holder) {
-
-		// Produce a string from holder[key].
+	var str = function (key, holder) {
 		var i, k, v, length, mind = gap, partial, value = holder[key];
 
-		// If the value has a toJSON method, call it to obtain a replacement value.
-		if (value && typeof value === $object && typeof value.toJSON === $function) { 
-			value = value[o.toJSON](key);
+		if (value && typeof value === 'object' && typeof value.toJSON === 'function') {
+			value = value.toJSON(key);
 		}
 
-		// If we were called with a replacer function, then call the replacer to
-		// obtain a replacement value.
-		if (typeof rep === $function) {
-				value = rep.call(holder, key, value);
+		if (typeof rep === 'function') {
+			value = rep.call(holder, key, value);
 		}
-
-		// What happens next depends on the value's type.
 
 		switch (typeof value) {
-			case $string: return quote(value);
-			case $number: return isFinite(value) ? String(value) : $null; // JSON numbers must be finite. Encode non-finite numbers as null.
-			case $boolean:
-			case $null: return String(value); // If the value is a boolean or null, convert it to a string. Note: typeof null does not produce 'null'. The case is included here in the remote chance that this gets fixed someday.
-			case $object: if (!value) { return $null; } // Due to a blunder in javascript, typeof null is 'object', so watch out for that case. If the type is 'object', we might be dealing with an object or an array or null.
+			case 'string': return quote(value);
+			case 'number': return isFinite(value) ? String(value) : 'null';
+			case 'boolean':
+			case 'null': return String(value);
+			case 'object': if (!value) {
+					return 'null';
+				}
 				gap += indent;
-				partial = []; // Make an array to hold the partial results of stringifying this object value.
-				if (Object.prototype.toString.apply(value) === '[object Array]') { // Is the value an array?
+				partial = [];
+				if ($$_language_prototypes_object.prototype.toString.apply(value) === '[object Array]') {
 					length = value.length;
-					// The value is an array. Stringify every element. Use null as a placeholder for non-JSON values.
 					for (i = 0; i < length; i += 1) {
-							partial[i] = str(i, value) || $null;
+						partial[i] = str(i, value) || 'null';
 					}
 
-					// Join all of the elements together, separated with commas, and wrap them in brackets.
-					v = partial.length === 0 ? '[]' : gap ? '[\n' + gap + partial.join(',\n' + gap) + '\n' + mind + ']' : '[' + partial.join(',') + ']'; 
+					v = partial.length === 0 ? '[]' : gap ? '[\n' + gap + partial.join(',\n' + gap) + '\n' + mind + ']' : '[' + partial.join(',') + ']';
 					gap = mind;
 					return v;
 				}
 
-
-				// If the replacer is an array, use it to select the members to be stringified.
-				if (rep && typeof rep === $object) {
+				if (rep && typeof rep === 'object') {
 					length = rep.length;
 					for (i = 0; i < length; i += 1) {
 						k = rep[i];
-						if (typeof k === $string) {
+						if (typeof k === 'string') {
 							v = str(k, value);
 							if (v) {
 								partial.push(quote(k) + (gap ? ': ' : ':') + v);
@@ -115,11 +99,8 @@
 						}
 					}
 				} else {
-
-					// Otherwise, iterate through all of the keys in the object.
-
 					for (k in value) {
-						if ($$_hasOwnProperty.call(value, k)) {
+						if (Object.hasOwnProperty.call(value, k)) {
 							v = str(k, value);
 							if (v) {
 								partial.push(quote(k) + (gap ? ': ' : ':') + v);
@@ -127,58 +108,29 @@
 						}
 					}
 				}
-
-// Join all of the member texts together, separated with commas,
-// and wrap them in braces.
-
-				v = partial.length === 0 ? '{}' : gap ? '{\n' + gap + partial.join(',\n' + gap) + '\n' + mind + '}' : '{' + partial.join(',') + '}'; 
 				gap = mind;
 				return v;
+		}
+	};
+
+	$$_json_stringify = $$_json.stringify = function (value, replacer, space) {
+		var i;
+		gap = '';
+		indent = '';
+
+		if (typeof space === 'number') {
+			for (i = 0; i < space; i += 1) {
+				indent += ' ';
 			}
-		};
+		} else if (typeof space === 'string') {
+			indent = space;
+		}
 
+		rep = replacer;
+		if (replacer && typeof replacer !== 'function' && (typeof replacer !== 'object' || typeof replacer.length !== 'number')) {
+			throw new Error('JSON.stringify');
+		}
 
-			$$_json_stringify = $$_json.stringify = function (value, replacer, space) {
-
-// The stringify method takes a value and an optional replacer, and an optional
-// space parameter, and returns a JSON text. The replacer can be a function
-// that can replace values, or an array of strings that will select the keys.
-// A default replacer method can be provided. Use of the space parameter can
-// produce text that is more easily readable.
-
-					var i;
-					gap = emptyString;
-					indent = emptyString;
-
-// If the space parameter is a number, make an indent string containing that
-// many spaces.
-
-					if (typeof space === $number) {
-							for (i = 0; i < space; i += 1) {
-									indent += ' ';
-							}
-
-// If the space parameter is a string, it will be used as the indent string.
-
-					} else if (typeof space === $string) {
-							indent = space;
-					}
-
-// If there is a replacer, it must be a function or an array.
-// Otherwise, throw an error.
-
-					rep = replacer;
-					if (replacer && typeof replacer !== $function &&
-									(typeof replacer !== $object ||
-									 typeof replacer.length !== $number)) {
-							//throw new Error('JSON.stringify');
-					}
-
-// Make a fake root object containing our value under the key of ''.
-// Return the result of stringifying the value.
-
-					return str(emptyString, {'': value});
-			};
-
-}());
-
+		return str('', {'': value});
+	};
+})();
