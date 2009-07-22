5d180aad9560eb293da463602568e2d623fc1e2e
robert <robert@oatlab.com>
2009-07-21 21:14:41 -0400

    removed consolelog

--- src/dom/event/get_element_from_point.js
+++ src/dom/event/get_element_from_point.js
@@ -2,7 +2,6 @@
 (function () {
 	var fn = function (e) {
 		return (fn = $$_dom_event_get_element_from_point = $$_dom_event.get_element_from_point = (!('pageX' in e) || !('x' in e)) ?  function (e) {
-			console.log('firefox / ie style');
 			return document.elementFromPoint(e.clientX,e.clientY);
 		} : function (e) {
 			return document.elementFromPoint(e.pageX,e.pageY);
