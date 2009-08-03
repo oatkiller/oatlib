d94dc86c1480b59ac9104b5f36ec8ea61416afd6
robert <robert@oatlab.com>
2009-08-03 17:22:56 -0400

    made map work (in its take form) on non array objects (non numeric even)

--- src/map.js
+++ src/map.js
@@ -1,11 +1,22 @@
-o.store(Array,'map',Array.prototype.map || function (fn) {
-	var that = this,
-	length = that.length,
-	response = new Array(length), i = 0;
+//= require <hasOwnProperty>
 
-	for (; i < length; i++) {
-		response[i] = fn.call(that,that[i],i,that);
-	}
-	return response;
+(function () {
 
-});
+	var fn = function (fn) {
+		var response = this.splice !== undefined ? [] : {},
+		property_name;
+
+		for (property_name in this) {
+			if (o.hasOwnProperty(this,property_name)) {
+				response[property_name] = fn.call(this,this[property_name],property_name,this);
+			}
+		}
+		return response;
+
+	});
+
+	// i want the native map implementation if that avail, but only for the version that sits on array prototype. for the take version i always want my own implementation because it needs to work for objects that dont have length or nueric properties etc
+	o.store(Array,'map',Array.prototype.map || fn);
+	o.map = fn;
+
+})();
--- units/map.tests.js
+++ units/map.tests.js
@@ -1,6 +1,6 @@
 test({
 	name: 'map',
-	'map': function () {
+	'works with arrays': function () {
 		var original = [2,4,6],
 		mapped = original[o.map](function (e) {
 				return e * 2;
@@ -9,5 +9,20 @@ test({
 		for (var i = 0, length = original.length; i < length; i++) {
 			Assert.areSame(original[i] * 2,mapped[i]);
 		}
+	},
+	'works with other objs': function () {
+		
+		var src = {
+			a: 1,
+			b: 2,
+			c: 3
+		},
+		result = o.map(src,function (value) {
+			return value * 2;
+		});
+
+		Assert.areSame(src.a * 2,result.a);
+		Assert.areSame(src.b * 2,result.b);
+		Assert.areSame(src.c * 2,result.c);
 	}
 });
