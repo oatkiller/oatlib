7c3d5a0dfca6f7894896028cc082aebeaf598052
robert <robert@oatlab.com>
2009-08-07 19:57:54 -0400

    increased default debounce time

--- src/dom/debounce.js
+++ src/dom/debounce.js
@@ -3,7 +3,7 @@
 //= require <dom/clear_timeout>
 o.dom.debounce = function (fn,threshold) {
 	var timer = false, args = arguments;
-	threshold = threshold || 50;
+	threshold = threshold || 110;
 	return function () {
 		timer && o.dom.clear_timeout(timer);
 		timer = o.dom.set_timeout(fn[o.bind](arguments),threshold);
