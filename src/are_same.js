2f6d8bc0373bedb62b74571d318cfad7747b80b7
robert <robert@oatlab.com>
2009-08-09 02:00:43 -0400

    this stuff is coming along

--- src/are_same.js
+++ src/are_same.js
@@ -1,7 +1,37 @@
 //= require <array>
-//= require <every>
-//= require <each>
-//= require <type_of>
-
-o.are_same = function (first) {
-};
+(function () {
+ 	var FAIL = {};
+	o.are_same = function () {
+		if (arguments.length < 2) {
+			return true;
+		}
+		var args = o.array(arguments);
+		var result = args[o.reduce](function (previous,current) {
+			if (previous === FAIL) {
+				return FAIL;
+			}
+			if (previous === current) {
+				return current;
+			}
+			var are_objs = previous !== null 
+				&& current !== null 
+				&& typeof previous === 'object'
+				&& typeof current === 'object'
+			if (are_objs) {
+				for (var x in current) {
+					if (!(x in previous)) {
+						return FAIL;
+					}
+				}
+				for (var x in previous) {
+				 if (!o.are_same(current[x],previous[x])) {
+						return FAIL;
+					}
+				}
+				return current;
+			}
+			return FAIL;
+	});
+		return result !== FAIL;
+	};
+})();
--- src/reduce.js
+++ src/reduce.js
@@ -1,6 +1,6 @@
 //= require <first_index>
 //= require <hasOwnProperty>
-o.store(Array,'reduce',Array.prototype.reduce || function (fn) {
+o.store(Array,'reduce',function (fn) {
 	var length = this.length, i = this[o.first_index](), memo = arguments.length > 1 ? arguments[1] : this[i];
 	for (; i < length; i++) {
 		if (o.hasOwnProperty(this,i)) {
