a8d7f768cc3c84fa1baac0e3d983c394c57173e2
robert <robert@oatlab.com>
2009-08-09 22:21:59 -0400

    syntax

--- src/dom/query_string_from_form.js
+++ src/dom/query_string_from_form.js
@@ -2,5 +2,5 @@
 //= require <dom/query_string_obj_from_form>
 //= require <remote/query_string_from_obj>
 o.dom.query_string_from_form = function (form_node) {
-	return o.remote.query_string_from_obj(o.dom.query_string_obj_from_form(form_node))
+	return o.remote.query_string_from_obj(o.dom.query_string_obj_from_form(form_node));
 };
