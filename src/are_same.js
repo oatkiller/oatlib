db2367e46c9dd3054c3e70e0f228a5dad0c1a750
robert <robert@oatlab.com>
2009-08-14 15:42:31 -0400

    dumb bug that happen cause of non reuse..

--- src/are_same.js
+++ src/are_same.js
@@ -7,7 +7,7 @@
 (function () {
  	var fail = {},
 	are_objects_or_arrays = function (ra) {
-		return ra[o.find](function (element) {return element.nodeType === undefined;}) && o.contains.apply(null,[['object','array']].concat(ra[o.map](o.type_of)));
+		return ra[o.find](function (element) {return element && element.nodeType === undefined;}) && o.contains.apply(null,[['object','array']].concat(ra[o.map](o.type_of)));
 		//return !ra[o.find](o.dom.is_node) && o.contains.apply(null,[['object','array']].concat(ra[o.map](o.type_of)));
 	};
 	o.are_same = function () {
