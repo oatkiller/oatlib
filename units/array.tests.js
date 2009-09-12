1c01bdc7bf3085fa1475d67f992e787bd144413a
robert <robert@oatlab.com>
2009-09-12 16:03:04 -0400

    yah

--- units/array.tests.js
+++ units/array.tests.js
@@ -9,6 +9,17 @@ test({
 		Assert.areSame('c',ra[2]);
 		Assert.areSame(3,ra.length);
 	},
+	'works for arguments': function () {
+		var my_fn = function () {
+			var ra = o.array(arguments);
+			Assert.isTrue(ra instanceof Array);
+			Assert.areSame(ra[0],arguments[0]);
+			Assert.areSame(ra[1],arguments[1]);
+			Assert.areSame(ra[2],arguments[2]);
+		};
+
+		my_fn(1,2,3);
+	},
 	'how does ie handle array elements with a value of undefined?': function () {
 		Assert.areSame(1,[undefined].length,'language is broken');
 		Assert.areSame(1,o.array([undefined]).length,'slice is broken?');
