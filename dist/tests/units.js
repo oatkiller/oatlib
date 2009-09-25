7f3a01a3c3d9b09352ec432189fddcaa52356eac
Robert Austin <robertaustin@Robert-Austins-MacBook-Pro.local>
2009-09-25 14:57:59 -0400

    dunno

--- dist/tests/units.js
+++ dist/tests/units.js
@@ -637,6 +637,17 @@ test({
 	}
 });
 test({
+	name: 'rgb_to_hsl',
+	'asdf': function () {
+		var rgb = [56,60,0],
+		expected_hsl = {h: 64, s: 100, l: 26},
+		hsl = o.color.rgb_to_hsl.apply(null,rgb);
+		Assert.areSame(hsl.h,expected_hsl.h);
+		Assert.areSame(hsl.s,expected_hsl.s);
+		Assert.areSame(hsl.l,expected_hsl.l);
+	}
+});
+test({
 	name: 'combine',
 	'combine': function () {
 		var newObj = o.combine({
