3d9a9c74741941565046c3d283699d16438c0731
Robert Austin <robert@oatlab.com>
2010-08-22 11:15:59 -0400

    removed unfinished color code

--- dist/tests/units.js
+++ dist/tests/units.js
@@ -1024,56 +1024,6 @@ Assert.areSame(6,[1,2,[3,4],[5,6]][o.flatten]().length);
 	}
 });
 test({
-	name: 'blog',
-	adsf: function () {
-String.prototype.isEmail = function () {
-	return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(this);
-};
-Assert.isTrue('robert@oatlab.com'.isEmail());
-Assert.isFalse('robertoatlab.com'.isEmail());
-Assert.isTrue('webmaster@del.icio.us'.isEmail());
-	},
-	blah: function () {
-String.prototype.isEmail = function () {
-	return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(this);
-};
-
-String.prototype.isEmail = function () {
-	return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/.test(this);
-};
-
-Assert.isTrue('robert@oatlab.com'.isEmail());
-Assert.isFalse('robertoatlab.com'.isEmail());
-Assert.isTrue('webmaster@del.icio.us'.isEmail(),'the .us one'); //  this will fail! the definition included by someone else overwrote yours, and doesn't match this address
-
-	},
-yeaaah: function () {
-var namespace = 'http://oatkiller.com/javascript',
-
-r = {},
-
-qualify = function (name) {
-	var qualified_name = r[name] = namespace + ':::' + name;
-
-	return qualified_name;
-};
-
-String.prototype[qualify('isEmail')] = function () {
-	return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(this);
-};
-
-
-String.prototype.isEmail = function () {
-	return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/.test(this);
-};
-
-Assert.isTrue('robert@oatlab.com'[r.isEmail]());
-Assert.isFalse('robertoatlab.com'[r.isEmail]());
-Assert.isTrue('webmaster@del.icio.us'[r.isEmail](),'the .us one');
-}
-
-});
-test({
 	name: 'application_event',
 	setUp: function () {
 		this.event = o.application_event();
@@ -2254,23 +2204,6 @@ test({
 			b: 2,
 			c: 3
 		}));
-	}
-});
-test({
-	name: 'arguments',
-	_should: {
-		ignore: 'asdf'
-	},
-	'asdf': function () {
-		var my_fn = function (a) {
-			Assert.areSame(0,a);
-			Assert.areSame(0,arguments[0]);
-			Assert.areSame(arguments[0],a);
-
-			a = 1;
-			Assert.areNotSame(a,arguments[0]);
-		};
-		my_fn(0);
 	}
 });
 test({
